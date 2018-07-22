f2d4cedc51d5daf3941d9d1b066df0b6f18425f9
Hadeel AlHulail <hadeel@Hadeels-MacBook-Pro.local>
2018-07-22 13:36:13 +0300

    Task12

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Restaurant, Item
 from .forms import RestaurantForm, ItemForm, SignupForm, SigninForm
 from django.contrib.auth import login, authenticate, logout
+from django.db.models import Q
 
 def no_access(request):
     return render(request, 'no_access.html')
@@ -46,11 +47,19 @@ def signout(request):
     return redirect("signin")
 
 def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        restaurants = restaurants.filter(
+            Q(name__icontains=query)|
+            Q(owner__username__icontains=query)
+            ).distinct()
+
     context = {
-        "restaurants":Restaurant.objects.all()
+        "restaurants": restaurants,
     }
     return render(request, 'list.html', context)
-
 
 def restaurant_detail(request, restaurant_id):
     restaurant = Restaurant.objects.get(id=restaurant_id)
